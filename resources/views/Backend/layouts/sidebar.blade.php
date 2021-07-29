1c96cdea771daae7e504084b2e164790619e982e
NizarDwiSetyawan <nizarsetyawan290@gmail.com>
2021-07-29 23:44:57 +0700

    Minggu-5

---
+++ resources/views/Backend/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+<!--sidebar start-->
+    <aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+          <li class="active">
+            <a class="" href="index.html">
+                          <i class="icon_house_alt"></i>
+                          <span>Dasbor</span>
+                      </a>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Formulir</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="form_component.html">bentuk elemen</a></li>
+              <li><a class="" href="form_validation.html">Validasi Formulir</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_desktop"></i>
+                          <span>UI Fitur</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="general.html">Elemen</a></li>
+              <li><a class="" href="buttons.html">Tombol</a></li>
+              <li><a class="" href="grids.html">Kisi</a></li>
+            </ul>
+          </li>
+          <li>
+            <a class="" href="widgets.html">
+                          <i class="icon_genius"></i>
+                          <span>Widget</span>
+                      </a>
+          </li>
+          <li>
+            <a class="" href="chart-chartjs.html">
+                          <i class="icon_piechart"></i>
+                          <span>Grafik</span>
+
+                      </a>
+
+          </li>
+
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_table"></i>
+                          <span>Tabel</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="basic_table.html">Tabel Dasar </a></li>
+            </ul>
+          </li>
+
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>Halaman</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="profile.html">Profil</a></li>
+              <li><a class="" href="login.html"><span>Halaman masuk</span></a></li>
+              <li><a class="" href="contact.html"><span>Halaman Kontak</span></a></li>
+              <li><a class="" href="blank.html">Halaman kosong</a></li>
+              <li><a class="" href="404.html">404 Error</a></li>
+            </ul>
+          </li>
+
+        </ul>
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
+    <!--sidebar end-->
